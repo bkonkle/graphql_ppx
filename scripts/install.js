186d293b90683395f80eedd44b25c7f21751eb9b
Brandon Konkle <brandon@ecliptic.io>
2018-02-05 17:22:01 -0700

    Another try

--- scripts/install.js
+++ scripts/install.js
@@ -15,8 +15,8 @@ function copy_binary(bin) {
     if (!fs.existsSync(ppx_path)) {
         console.log(`[graphql_ppx] Copying binary from "${bin}" to "${ppx_path}"`)
         fs.symlinkSync(bin, ppx_path)
-        fs.chmodSync(ppx_path, 0555)
     }
+    fs.chmodSync(ppx_path, 0555)
 }
 
 
